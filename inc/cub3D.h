69a5a91d6a03729e9e8c88211f8e5c5a919e6aa0
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-09 14:32:49 +0100

    main header of the project added

---
+++ inc/cub3D.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 12:27:58 by efinda            #+#    #+#             */
+/*   Updated: 2025/01/09 13:52:19 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <math.h>
+# include "mlx.h"
+# include "struct.h"
+# include "../libft/libft.h"
+
+# define ESC        65307
+# define LEFT       65362
+# define RIGHT      65364
+# define A_KEY      65
+# define D_KEY      68
+# define S_KEY      83
+# define W_KEY      87
+
+
+
+#endif
