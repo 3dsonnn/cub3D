acebc61ebe35f42fa9a89d862496cda670676cf1
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-10 02:30:12 +0100

    updated

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -6,35 +6,35 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 12:27:58 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/09 21:38:35 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/10 02:27:53 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <sys/time.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <errno.h>
-# include <math.h>
+# include "../libft/libft.h"
 # include "mlx.h"
 # include "struct.h"
-# include "../libft/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
 
-# define ESC        65307
-# define LEFT       65362
-# define RIGHT      65364
-# define A_KEY      65
-# define D_KEY      68
-# define S_KEY      83
-# define W_KEY      87
+# define ESC 65307
+# define LEFT 65362
+# define RIGHT 65364
+# define A_KEY 65
+# define D_KEY 68
+# define S_KEY 83
+# define W_KEY 87
 
 //  CHECKS
-void    checks(t_cub *cub, int ac, char **av);
-void	check_elements(t_cub *cub, int i);
+void	checks(t_cub *cub, int ac, char **av);
+int		check_element(t_cub *cub, int error);
 
 #endif
--- inc/struct.h
+++ inc/struct.h
@@ -6,7 +6,7 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:50:36 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/10 01:06:57 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/10 02:18:46 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 
 typedef struct s_wall
 {
-    int     fd;
-	char    *ID;
-    char    *path;
-}		t_wall;
+	int		fd;
+	char	*ID;
+	char	*path;
+}			t_wall;
 
 typedef struct s_fc
 {
-    int     fd;
-	char    *ID;
-    char    *range;
-    int     r;
-    int     g;
-    int     b;
-}		t_fc;
+	int		fd;
+	char	*ID;
+	char	*range;
+	int		r;
+	int		g;
+	int		b;
+}			t_fc;
 
 typedef struct s_map
 {
-    char    **map;
-    t_fc    fc[2];
-    t_wall  walls[4];
-}   t_map;
+	char	**map;
+	t_fc	fc[2];
+	t_wall	walls[4];
+}			t_map;
 
 typedef struct s_img
 {
@@ -44,16 +44,16 @@ typedef struct s_img
 	int		bpp;
 	int		line_len;
 	int		endian;
-}		t_img;
+}			t_img;
 
 typedef struct s_cub
 {
-	int     fd;
-    char    *line;
-    char    *tmp;
-    char    **mtx;
-    t_img   img;
-    t_map   map;
-}		t_cub;
+	int		fd;
+	char	*line;
+	char	*tmp;
+	char	**mtx;
+	t_img	img;
+	t_map	map;
+}			t_cub;
 
 #endif
