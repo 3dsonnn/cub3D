c0ce0ffc62f0dcd84f5b6a8e5b5306643ecebe82
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-11 11:23:43 +0100

    updated

--- src/checks/checks.c
+++ src/checks/checks.c
@@ -6,7 +6,7 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:00:49 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/10 04:11:27 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/11 10:37:09 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 static	void	check_args(t_scene *scene, int ac, char **av)
 {
 	if (ac != 2)
-		exit_error("Invalid number of arguments", NULL, NULL);
+		exit_error("Invalid number of arguments", scene);
 	if (ft_strlen(av[1]) <= 4 || ft_strncmp(av[1] + ft_strlen(av[1]) - 4,
 			".cub", 4))
-		exit_error("Invalid scene file extension", NULL, NULL);
+		exit_error("Invalid scene file extension", scene);
 	scene->fd = open(av[1], O_RDONLY);
 	if (scene->fd < 0)
-		exit_error(strerror(errno), NULL, NULL);
+		exit_error(strerror(errno), scene);
 	scene->line = get_next_line(scene->fd);
 	if (!scene->line)
-		exit_error("Empty scene file", NULL, NULL);
+		exit_error("Empty scene file", scene);
 	ft_strfree(&scene->line);
 }
 
@@ -34,7 +34,7 @@ static	void	check_elements(t_scene *scene, int i)
 	{
 		scene->line = get_next_line(scene->fd);
 		if (!scene->line)
-			exit_error("The scene file is incomplete", NULL, NULL);
+			exit_error("Missing elements in the scene file", scene);
 		scene->line[ft_strlen(scene->line) - 1] = '\0';
 		if (!*scene->line || ft_strspace(scene->line))
 		{
@@ -45,9 +45,9 @@ static	void	check_elements(t_scene *scene, int i)
         i++;
 	}
 	if (i != 6)
-		exit_error("Missing elements in the scene file", NULL, NULL);
+		exit_error("Missing elements in the scene file", scene);
 }
-
+/*
 static	void	check_map(t_scene *scene)
 {
 	char	*line;
@@ -74,10 +74,16 @@ static	void	check_map(t_scene *scene)
 		exit(1);
 	}
 }
-
+*/
 void	checks(t_scene *scene, int ac, char **av)
 {
+	scene->tmp = NULL;
+	scene->line = NULL;
+	scene->mtx = NULL;
+	scene->map = NULL;
+	scene->elements = NULL;
 	check_args(scene, ac, av);
 	check_elements(scene, 0);
+	ft_strfree(&scene->elements);
 	check_map(scene);
 }
