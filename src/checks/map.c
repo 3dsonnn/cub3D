4791d5c7963df1e33f7622f9c1f3fb73bd31ff5b
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-11 20:48:25 +0100

    files containing functions that will validate the map

--- src/checks/map.c
+++ src/checks/map.c
@@ -6,8 +6,47 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 02:23:44 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/11 09:22:45 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/11 19:28:44 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
+
+void	escape_empty_lines(t_scene *scene)
+{
+	ft_strfree(&scene->line);
+	while (-42)
+	{
+		scene->line = get_next_line(scene->fd);
+		if (!scene->line)
+			exit_error("The map is missing in the scene file", scene);
+		scene->line[ft_strlen(scene->line) - 1] = '\0';
+		if (!*scene->line || ft_strspace(scene->line))
+		{
+			ft_strfree(&scene->line);
+			continue ;
+		}
+        break ;
+	}
+}
+
+void	fill_map(t_scene *scene, t_map *map)
+{
+	while (-42)
+	{
+		scene->line = get_next_line(scene->fd);
+		if (!scene->line)
+			break ;
+		scene->line[ft_strlen(scene->line) - 1] = '\0';
+		if (!*scene->line || ft_strspace(scene->line))
+			exit_error("There cannot be empty lines inside the map content", scene);
+		if (ft_strcspn(scene->line, "01 NSEW") != ft_strlen(scene->line))
+			exit_error("Invalid character inside the map", scene);
+        scene->mtx = ft_mtxdup(map->content);
+        ft_mtxfree(&map->content);
+        ft_addstr_mtx(&map->content, scene->mtx, scene->line);
+        ft_strfree(&scene->line);
+	}
+    map->rows = ft_mtxlen(map->content);
+    map->cols = ft_longestr_mtx(map->content);
+}
