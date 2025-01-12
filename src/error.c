35306445dd19cc6de930b0e4db25ee811879dea6
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-12 11:38:07 +0100

    had to give the return of the write function to an int and then (void) it cuz Wall Wextra Werror was making noise

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 16:22:09 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/11 17:38:01 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/12 07:11:01 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void    exit_error(char *message, t_scene *scene)
 {
-    write(2, "Error\n", 6);
-    write(2, message, ft_strlen(message));
-    write(2, "\n", 1);
+    int return_value;
+
+    return_value = write(2, "Error\n", 6);
+    return_value = write(2, message, ft_strlen(message));
+    return_value = write(2, "\n", 1);
     ft_strfree(&scene->tmp);
     ft_strfree(&scene->line);
     ft_strfree(&scene->elements);
     ft_mtxfree(&scene->mtx);
     ft_mtxfree(&scene->map.content);
+    (void)return_value;
 	exit(1);
 }
