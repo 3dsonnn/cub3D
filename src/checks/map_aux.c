00d082a71820d2034e71d07d269f9da61f5e0c75
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-12 19:17:32 +0100

    created two functions here to fill all the 1's considerated on the boards of the map

--- src/checks/map_aux.c
+++ src/checks/map_aux.c
@@ -6,86 +6,57 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 02:12:28 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/12 05:39:47 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/12 19:14:31 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
 
-static void	free_visited(t_map *map)
+static	void	fill_diagonal(t_map *map, int x, int y)
 {
-	int	i;
-
-	i = -1;
-	while (++i < map->size.y)
-		free(map->visited[i]);
-	free(map->visited);
-}
-
-static bool	is_on_border(t_map *map)
-{
-	return (map->neighbor.x == 0 || map->neighbor.x == map->size.x - 1
-		|| map->neighbor.y == 0 || map->neighbor.y == map->size.y - 1);
-}
-
-static bool	has_space_nearby(t_map *map, t_point pos)
-{
-	int	d;
-
-	d = -1;
-	while (++d < 4)
+	while (++y < map->size.y - 1)
 	{
-		map->nx = pos.x + map->dx[d];
-		map->ny = pos.y + map->dy[d];
-		if (map->nx >= 0 && map->nx < map->size.x && map->ny >= 0
-			&& map->ny < map->size.y)
-			if (map->content[map->ny][map->nx] == ' ')
-				return (true);
-	}
-	return (false);
-}
-
-static void	my_flood_fill_aux(t_map *map, t_point *stack, int *top)
-{
-	int	d;
-
-	d = -1;
-	while (++d < 4)
-	{
-		map->nx = map->cur.x + map->dx[d];
-		map->ny = map->cur.y + map->dy[d];
-		if (map->nx >= 0 && map->nx < map->size.x && map->ny >= 0
-			&& map->ny < map->size.y && !map->visited[map->ny][map->nx]
-			&& map->content[map->ny][map->nx] == '1')
+		x = -1;
+		while (++x < map->size.x - 1)
 		{
-			map->neighbor = (t_point){map->nx, map->ny};
-			if (is_on_border(map) || has_space_nearby(map, map->neighbor))
+			if (map->content[y][x] == ' ')
 			{
-				stack[++(*top)] = map->neighbor;
-				map->visited[map->ny][map->nx] = true;
+				if (y > 0 && x > 0 && map->content[y - 1][x - 1] == '1')
+					map->content[y - 1][x - 1] = '*';
+				if (y > 0 && x < map->size.x - 1 && map->content[y - 1][x
+					+ 1] == '1')
+					map->content[y - 1][x + 1] = '*';
+				if (y < map->size.y - 1 && x > 0 && map->content[y + 1][x
+					- 1] == '1')
+					map->content[y + 1][x - 1] = '*';
+				if (y < map->size.y - 1 && x < map->size.x - 1 && map->content[y
+					+ 1][x + 1] == '1')
+					map->content[y + 1][x + 1] = '*';
 			}
 		}
 	}
 }
 
-bool	my_flood_fill(t_map *map)
+void	my_flood_fill(t_map *map, int x, int y)
 {
-	int		top;
-	t_point	stack[map->size.x * map->size.y];
-
-	top = -1;
-	stack[++top] = map->init;
-	map->visited[map->init.y][map->init.x] = true;
-	while (top >= 0)
+	while (++y < map->size.y - 1)
 	{
-		map->cur = stack[top--];
-		if (map->cur.x == map->init.x && map->cur.y == map->init.y && top != -1)
+		x = -1;
+		while (++x < map->size.x - 1)
 		{
-			free_visited(map);
-			return (true);
+			if (map->content[y][x] == ' ')
+			{
+				if (y > 0 && map->content[y - 1][x] == '1')
+					map->content[y - 1][x] = '*';
+				if (y < map->size.y - 1 && map->content[y + 1][x] == '1')
+					map->content[y + 1][x] = '*';
+				if (x > 0 && map->content[y][x - 1] == '1')
+					map->content[y][x - 1] = '*';
+				if (x < map->size.x - 1 && map->content[y][x + 1] == '1')
+					map->content[y][x + 1] = '*';
+			}
 		}
-		my_flood_fill_aux(map, stack, &top);
 	}
-	free_visited(map);
-	return (false);
+	fill_diagonal(map, -1, -1);
 }
+
