7ca90132bd8d069322c4ec2d01bc85dd03db07fe
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-12 01:31:55 +0100

    added a function to check if there's a valid starting position and another one to basically put spaces on the imcomplete rows of the map

--- src/checks/map.c
+++ src/checks/map.c
@@ -6,27 +6,30 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 02:23:44 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/11 19:28:44 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/12 00:18:01 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
 
-void	escape_empty_lines(t_scene *scene)
+static	void	fill_map_aux(t_scene *scene, t_map *map)
 {
-	ft_strfree(&scene->line);
-	while (-42)
+	int i;
+	int	len;
+
+	i = -1;
+	while (++i < map->size.y)
 	{
-		scene->line = get_next_line(scene->fd);
-		if (!scene->line)
-			exit_error("The map is missing in the scene file", scene);
-		scene->line[ft_strlen(scene->line) - 1] = '\0';
-		if (!*scene->line || ft_strspace(scene->line))
+		len = ft_strlen(map->content[i]);
+		if (len < map->size.x)
 		{
-			ft_strfree(&scene->line);
-			continue ;
+			scene->tmp = ft_calloc(map->size.x + 1, sizeof(char));
+			ft_strnfill(scene->tmp, map->content[i], len);
+			ft_memset(scene->tmp + len, ' ', map->size.x - len);
+			ft_strfree(&map->content[i]);
+			map->content[i] = scene->tmp;
+			scene->tmp = NULL;
 		}
-        break ;
 	}
 }
 
@@ -47,6 +50,51 @@ void	fill_map(t_scene *scene, t_map *map)
         ft_addstr_mtx(&map->content, scene->mtx, scene->line);
         ft_strfree(&scene->line);
 	}
-    map->rows = ft_mtxlen(map->content);
-    map->cols = ft_longestr_mtx(map->content);
+    map->size.x = ft_longestr_mtx(map->content);
+    map->size.y = ft_mtxlen(map->content);
+	fill_map_aux(scene, map);
+}
+
+void	escape_empty_lines(t_scene *scene)
+{
+	ft_strfree(&scene->line);
+	while (-42)
+	{
+		scene->line = get_next_line(scene->fd);
+		if (!scene->line)
+			exit_error("The map is missing in the scene file", scene);
+		scene->line[ft_strlen(scene->line) - 1] = '\0';
+		if (!*scene->line || ft_strspace(scene->line))
+		{
+			ft_strfree(&scene->line);
+			continue ;
+		}
+        break ;
+	}
+} 
+
+void	check_starting_position(t_scene *scene, t_map *map)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	i = -1;
+	flag = 0;
+	while (++i < map->size.y)
+	{
+		j = -1;
+		while (++j < map->size.x)
+		{
+			if (ft_strchr("NSEW", map->content[i][j]))
+			{
+				map->start = map->content[i][j];
+				flag++;
+			}
+		}
+	}
+	if (!flag)
+		exit_error("Invalid map: there isn't a start position", scene);
+	if (flag > 1)
+		exit_error("Invalid map: there's more than one start position", scene);
 }
