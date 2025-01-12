f13abc24cae5f302365c0a337edcf8babd452b67
Welepy-dev <mwsilva.contato@gmail.com>
2025-01-12 10:16:24 +0000

    fix: rename parameter for clarity in ft_lstadd_back function

--- libft/linked_list/ft_lstadd_back.c
+++ libft/linked_list/ft_lstadd_back.c
@@ -6,21 +6,21 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 20:53:07 by efinda            #+#    #+#             */
-/*   Updated: 2024/05/21 20:53:16 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/12 09:26:56 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *newnode)
 {
 	t_list	*tmp;
 
 	if (!(*lst))
-		*lst = new;
+		*lst = newnode;
 	else
 	{
 		tmp = ft_lstlast(*lst);
-		tmp->next = new;
+		tmp->next = newnode;
 	}
 }
