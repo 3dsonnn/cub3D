373b7ca16fb7f287fa5b6c23e3d247b35745e4b7
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-15 02:28:21 +0100

    the first function returns the rgb value of a color and the second one is my_mlx_pixel_put to, of course, put pixels on an image

---
+++ src/my_mlx/my_mlx_utils.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 22:53:50 by efinda            #+#    #+#             */
+/*   Updated: 2025/01/15 00:25:14 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3D.h"
+
+inline int	mlx_get_rgb_color(int r, int g, int b)
+{
+	return ((r << 16) | (g << 8) | b);
+}
+
+void	my_mlx_pixel_put(t_img *image, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = image->addr + (y * image->line_len) + x * (image->bpp / 8);
+	*(unsigned int *)(dst) = color;
+}
