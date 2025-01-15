91b6699708202ccd55426ad3108c9b7c4bcb3adb
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-01-15 11:40:43 +0100

    updated

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:58:45 by efinda            #+#    #+#             */
-/*   Updated: 2025/01/15 02:58:09 by efinda           ###   ########.fr       */
+/*   Updated: 2025/01/15 11:18:53 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int main(int ac, char **av)
     checks(&cub.scene, ac, av);
     my_mlx_init(&cub);
     my_mlx_hook(&cub);
+	cub3D(&cub);
 	mlx_loop(cub.mlx);
     return (0);
 }
