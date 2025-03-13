483312724f0315f9ea2206345d8fe5b17d19e5ea
Edson Finda <efinda@c2r2s2.42luanda.com>
2025-03-13 02:37:23 +0100

    now the norm on the minimap_bonus file is done and the functions are way more well structured

--- bonus/src/minimap/minimap_bonus.c
+++ bonus/src/minimap/minimap_bonus.c
@@ -6,64 +6,66 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 22:59:10 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/13 02:22:01 by efinda           ###   ########.fr       */
+/*   Updated: 2025/03/13 02:35:52 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D_bonus.h"
 
-static  inline  int map(int old_value, int old_min, int old_max, int new_min, int new_max)
+static inline int	map(int old_value, int old_limits[2], int new_limits[2])
 {
-    double old_range = old_max - old_min;
-    double new_range = new_max - new_min;
-    double ratio = (double)(old_value - old_min) / old_range;
-    return (int)(new_min + ratio * new_range);
+	double	old_range;
+	double	new_range;
+	double	ratio;
+
+	old_range = old_limits[1] - old_limits[0];
+	new_range = new_limits[1] - new_limits[0];
+	ratio = (double)(old_value - old_limits[0]) / old_range;
+	return ((int)(new_limits[0] + ratio * new_range));
 }
 
-static void miniplayer(t_cub *cub, t_dpoint player_pos, t_point cur_crd)
+static void	miniplayer(t_cub *cub, t_point base, t_point tile_min)
 {
-    t_bresenham_line line;
-    line.img = &cub->img;
-    
-    int base_x = cur_crd.x * cub->minimap.tilesize + 10;
-    int base_y = cur_crd.y * cub->minimap.tilesize + 10;
+	t_bresenham_line	line;
 
-    int tile_x_min = ((int)cub->player.pos.x / TILE) * TILE;
-    int tile_y_min = ((int)cub->player.pos.y / TILE) * TILE;
-
-    line.crd.x0 = map((int)cub->player.pos.x, tile_x_min, tile_x_min + TILE, 
-                      base_x, base_x + cub->minimap.tilesize);
-    line.crd.y0 = map((int)cub->player.pos.y, tile_y_min, tile_y_min + TILE, 
-                      base_y, base_y + cub->minimap.tilesize);
-    line.crd.x = line.crd.x0 + cub->player.dir.x * 10;
-    line.crd.y = line.crd.y0 + cub->player.dir.y * 10;
-    bresenham_line(line);
-    line.crd.x0++;
-    line.crd.x++;
-    bresenham_line(line);
-    line.crd.y0++;
-    line.crd.y++;
-    bresenham_line(line);
+	line.img = &cub->img;
+	line.crd.x0 = map((int)cub->player.pos.x, (int[]){tile_min.x, tile_min.x
+			+ TILE}, (int[]){base.x, base.x + cub->minimap.tilesize});
+	line.crd.y0 = map((int)cub->player.pos.y, (int[]){tile_min.y, tile_min.y
+			+ TILE}, (int[]){base.y, base.y + cub->minimap.tilesize});
+	line.crd.x = line.crd.x0 + cub->player.dir.x * 10;
+	line.crd.y = line.crd.y0 + cub->player.dir.y * 10;
+	bresenham_line(line);
+	line.crd.x0++;
+	line.crd.x++;
+	bresenham_line(line);
+	line.crd.y0++;
+	line.crd.y++;
+	bresenham_line(line);
 }
 
-void    minimap(t_cub *cub, int i, int j)
+void	minimap(t_cub *cub, int i, int j)
 {
-    if (cub->minimap.box)
-        paint_obx(cub, cub->minimap.corners[TOPLEFT]);
-    else
-    {
-        i = -1;
+	if (cub->minimap.box)
+		paint_obx(cub, cub->minimap.corners[TOPLEFT]);
+	else
+	{
+		i = -1;
 		while (++i < cub->scene.map.size.y)
 		{
 			j = -1;
 			while (++j < cub->scene.map.size.x)
-            {
-                if (cub->minimap.tiles[i][j].id == ' ')
-				    ;
-                else
-				    paint_minimap_tile(cub, i, j, cub->minimap.tiles[i][j].color);
-            }
+			{
+				if (cub->minimap.tiles[i][j].id == ' ')
+					;
+				else
+					paint_minimap_tile(cub, i, j,
+						cub->minimap.tiles[i][j].color);
+			}
 		}
-    }
-    miniplayer(cub, cub->player.pos, cub->minimap.cur->crd);
+	}
+	miniplayer(cub, (t_point){cub->minimap.cur->crd.x * cub->minimap.tilesize
+		+ 10, cub->minimap.cur->crd.y * cub->minimap.tilesize + 10},
+		(t_point){((int)cub->player.pos.x / TILE) * TILE,
+		((int)cub->player.pos.y / TILE) * TILE});
 }
