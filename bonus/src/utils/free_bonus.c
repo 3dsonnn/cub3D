3d2ee7c2b027e5f58b90dfba1102fc0c79eb74f2
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-04-02 00:02:21 +0100

    updated

--- bonus/src/utils/free_bonus.c
+++ bonus/src/utils/free_bonus.c
@@ -6,19 +6,33 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:47:58 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/12 15:55:34 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/01 16:54:19 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D_bonus.h"
 
-void    free_tiles(t_tile ***tiles, int i, int size)
+void	free_tiles(t_tile ***tiles, int i, int size)
 {
-    if (!tiles || !*tiles || !(*tiles)[0])
-        return ;
+	if (!tiles || !*tiles || !(*tiles)[0])
+		return ;
 	while (++i < size)
-        if ((*tiles)[i])
-            free((*tiles)[i]);
-    free(*tiles);
-    *tiles = NULL;
+		if ((*tiles)[i])
+			free((*tiles)[i]);
+	free(*tiles);
+	*tiles = NULL;
+}
+
+void	destroy_all_imgs(t_cub *cub)
+{
+	int	i;
+
+	i = -1;
+	my_mlx_destroy_img(cub->mlx, &cub->img);
+	while (++i < 4)
+		my_mlx_destroy_img(cub->mlx, &cub->scene.textures[i].img);
+	my_mlx_destroy_img(cub->mlx, &cub->minimap.img);
+	i = -1;
+	while (++i < 27)
+		my_mlx_destroy_img(cub->mlx, &cub->sprites.imgs[i]);
 }
