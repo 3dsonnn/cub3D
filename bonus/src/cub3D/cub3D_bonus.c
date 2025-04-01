425174f61d430d71e1e8ee0198ba8fe4ae58012e
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-04-02 00:34:08 +0100

    instead of painting the health bar into the main image I've created an image that represents the bar and I put this image into the window after putting the main image, it makes the renderization more soft

--- bonus/src/cub3D/cub3D_bonus.c
+++ bonus/src/cub3D/cub3D_bonus.c
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:20:38 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/25 02:25:31 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/01 19:53:57 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,7 @@ void	cub3D(t_cub *cub)
 	minimap(cub);
 	player(cub);
 	mlx_put_image_to_window(cub->mlx, cub->win, cub->img.img, 0, 0);
+	mlx_put_image_to_window(cub->mlx, cub->win,
+		cub->sprites.imgs[HEALTH_BAR].img, 20, HEIGHT - 20
+		- cub->sprites.imgs[HEALTH_BAR].height);
 }
--- bonus/src/sprites/init_bonus.c
+++ bonus/src/sprites/init_bonus.c
@@ -6,17 +6,52 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 09:50:57 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/29 11:04:05 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/01 18:23:27 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D_bonus.h"
 
-static int	get_clear_img(t_cub *cub, t_img *img, int i)
+static void	paint_health_bar_img_dfl(t_img *img)
 {
-	if (i != 24)
-		return (0);
-	img->img = mlx_new_image(cub->mlx, GUN_WIDTH, GUN_HEIGHT);
+	my_mlx_draw_horizontal_lines_to_img(img, (t_point){3, 3},
+		(t_point){HEALTH_BAR_WIDTH - 6, HEALTH_BAR_HEIGHT - 6}, RED);
+	my_mlx_draw_horizontal_lines_to_img(img, (t_point){0, 0},
+		(t_point){HEALTH_BAR_WIDTH, 3}, WHITE);
+	my_mlx_draw_horizontal_lines_to_img(img, (t_point){0, img->height - 3},
+		(t_point){HEALTH_BAR_WIDTH, 3}, WHITE);
+	my_mlx_draw_horizontal_lines_to_img(img, (t_point){0, 0}, (t_point){3,
+		HEALTH_BAR_HEIGHT}, WHITE);
+	my_mlx_draw_horizontal_lines_to_img(img, (t_point){img->width - 3, 0},
+		(t_point){3, HEALTH_BAR_HEIGHT}, WHITE);
+}
+
+static int	get_health_bar_img(t_cub *cub, t_img *img)
+{
+	my_mlx_new_img(cub->mlx, img, (t_point){HEALTH_BAR_WIDTH,
+		HEALTH_BAR_HEIGHT});
+	if (!img->img)
+	{
+		get_explicit_error_message(&cub->scene,
+			(t_strs){"Failed to create the health bar image", NULL, NULL, NULL,
+			NULL, NULL});
+		return (1);
+	}
+	my_mlx_get_data_addr(img);
+	if (!img->addr)
+	{
+		get_explicit_error_message(&cub->scene,
+			(t_strs){"Failed to get the address of the health bar image", NULL,
+			NULL, NULL, NULL, NULL});
+		return (1);
+	}
+	paint_health_bar_img_dfl(img);
+	return (0);
+}
+
+static int	get_clear_img(t_cub *cub, t_img *img)
+{
+	my_mlx_new_img(cub->mlx, img, (t_point){GUN_WIDTH, GUN_HEIGHT});
 	if (!img->img)
 	{
 		get_explicit_error_message(&cub->scene,
@@ -24,21 +59,18 @@ static int	get_clear_img(t_cub *cub, t_img *img, int i)
 			NULL});
 		return (1);
 	}
-	img->addr = (int *)mlx_get_data_addr(img->img, &img->bpp, &img->line_len,
-			&img->endian);
+	my_mlx_get_data_addr(img);
 	if (!img->addr)
 	{
-		mlx_destroy_image(cub->mlx, img->img);
 		get_explicit_error_message(&cub->scene,
 			(t_strs){"Failed to get the address of the clear image", NULL, NULL,
 			NULL, NULL, NULL});
 		return (1);
 	}
-	img->line_len /= 4;
-	return (0);
+	return (get_health_bar_img(cub, &cub->sprites.imgs[HEALTH_BAR]));
 }
 
-static int	my_mlx_xpm_file_to_image(t_cub *cub, t_img *img, char *path)
+static int	sprite_xpm_file_to_img(t_cub *cub, t_img *img, char *path)
 {
 	if (!path)
 		return (1);
@@ -50,52 +82,40 @@ static int	my_mlx_xpm_file_to_image(t_cub *cub, t_img *img, char *path)
 			"Make sure the configuration files were not modified", NULL, NULL});
 		return (1);
 	}
-	img->addr = (int *)mlx_get_data_addr(img->img, &img->bpp, &img->line_len,
-			&img->endian);
+	my_mlx_get_data_addr(img);
 	if (!img->addr)
 	{
-		mlx_destroy_image(cub->mlx, img->img);
 		get_explicit_error_message(&cub->scene,
 			(t_strs){"Failed to get the address of image from ", path, ".\n",
 			"Make sure the configuration files were not modified", NULL, NULL});
 		return (1);
 	}
-	img->line_len /= 4;
 	return (0);
 }
 
-void	get_frames_images(t_cub *cub, int i)
+void	get_sprites_images(t_cub *cub, int i)
 {
-	const char	*paths[25] = PATHS;
+	const char	*paths[25] = {CROSSHAIR_PATH, CARTRIDGE_PATH, IDLE_PATH,
+			PUTTING_01_PATH, PUTTING_02_PATH, PUTTING_03_PATH,
+			RECHARGING_01_PATH, RECHARGING_02_PATH, RECHARGING_03_PATH,
+			RECHARGING_04_PATH, RECHARGING_05_PATH, RECHARGING_06_PATH,
+			RECHARGING_IDLE_PATH, SHELL_01_PATH, SHELL_02_PATH, SHELL_03_PATH,
+			SHELL_04_PATH, SHELL_05_PATH, SHELL_06_PATH, SHELL_07_PATH,
+			SHELL_08_PATH, SHELL_09_PATH, SHOOTING_01_PATH, SHOOTING_02_PATH,
+			SHOOTING_IDLE_PATH};
 
 	while (++i < 25)
 	{
-		if (get_clear_img(cub, &cub->sprites.frames[i], i)
-			|| my_mlx_xpm_file_to_image(cub, &cub->sprites.frames[i],
+		if (sprite_xpm_file_to_img(cub, &cub->sprites.imgs[i],
 				(char *)paths[i]))
-		{
-			while (--i >= 0)
-				mlx_destroy_image(cub->mlx, cub->sprites.frames[i].img);
-			my_mlx_free(cub, cub->scene.tmp, (t_plane){-1, 4, 0, 1});
-		}
-		if (!i)
-			cub->sprites.frames[i] = my_mlx_resize_img(cub->mlx,
-					cub->sprites.frames[i], (t_point){16, 16});
+			my_mlx_error_free(cub, cub->scene.tmp);
+		if (i < 2)
+			my_mlx_resize_img(cub->mlx, &cub->sprites.imgs[i], (t_point){16 + (i
+					* 16), 16 + (i * 16)});
 		else
-			cub->sprites.frames[i] = my_mlx_resize_img(cub->mlx,
-					cub->sprites.frames[i], (t_point){GUN_WIDTH, GUN_HEIGHT});
+			my_mlx_resize_img(cub->mlx, &cub->sprites.imgs[i],
+				(t_point){GUN_WIDTH, GUN_HEIGHT});
 	}
-}
-
-void	init_sprites(t_cub *cub)
-{
-    cub->sprites.cub = cub;
-    cub->sprites.ammo = 8;
-    cub->sprites.health = (t_nbr){.value = 100, .str = NULL};
-	cub->sprites.idle = true;
-	cub->sprites.shooting = false;
-	cub->sprites.recharging = false;
-	cub->sprites.last_frame_time = 0;
-	cub->sprites.cur_frame_index = IDLE;
-    pthread_create(&cub->sprites.thread, NULL, );
+	if (get_clear_img(cub, &cub->sprites.imgs[CLEAR]))
+		my_mlx_error_free(cub, cub->scene.tmp);
 }
