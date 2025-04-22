c14a263be3a84f543f0e813aaf16f3b9129073b0
welepy-dev <mwsilva.contato@gmail.com>
2025-04-22 15:09:48 +0100

    commenting unnecessary code

--- bonus/src/cub3D/cub3D_bonus.c
+++ bonus/src/cub3D/cub3D_bonus.c
@@ -6,7 +6,7 @@
 /*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 11:20:38 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/27 13:06:11 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/03/28 12:05:49 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	cub3D(t_cub *cub)
 	get_rays(cub, -1);
 	minimap(cub);
 	player(cub);
-	mlx_put_image_to_window(cub->mlx, cub->win, cub->img.img, 0, 0);
+	// mlx_put_image_to_window(cub->mlx, cub->win, cub->img.img, 0, 0);
 }
