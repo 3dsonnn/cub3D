44e7e8c3b0b3f20631ade863916b03a178c0b1c8
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-04-24 15:20:15 +0100

    Just did copy/paste of the new way I'm validating the scene file on the mandatory part and added all the files and functions that helps me doing it

--- bonus/src/checks/textures_bonus.c
+++ bonus/src/checks/textures_bonus.c
@@ -6,60 +6,61 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 22:22:11 by efinda            #+#    #+#             */
-/*   Updated: 2025/04/22 10:54:37 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/24 14:38:51 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D_bonus.h"
 
-static void	fill_texture(t_scene *scene, char ID, char *path)
+static void	fill_texture(t_scene *scene, char id, char *path)
 {
 	if (!scene->elements)
-		scene->elements = ft_strdup((char []){ID, '\0'});
+		scene->elements = ft_strdup((char []){id, '\0'});
 	else
-		check_duplicate_id(scene, ID);
-	if (ID == 'N')
-		scene->textures[NO] = (t_texture){.id = NO, .path = ft_strdup(path)};
-	else if (ID == 'S')
-		scene->textures[SO] = (t_texture){.id = SO, .path = ft_strdup(path)};
-	else if (ID == 'W')
-		scene->textures[WE] = (t_texture){.id = WE, .path = ft_strdup(path)};
-	else if (ID == 'E')
-		scene->textures[EA] = (t_texture){.id = EA, .path = ft_strdup(path)};
+		check_duplicate_id(scene, id);
+	if (id == 'N')
+		scene->textures[NO].path = ft_strdup(path);
+	else if (id == 'S')
+		scene->textures[SO].path = ft_strdup(path);
+	else if (id == 'W')
+		scene->textures[WE].path = ft_strdup(path);
+	else if (id == 'E')
+		scene->textures[EA].path = ft_strdup(path);
 }
 
-static void	check_texture_aux(t_scene *scene, char *ID)
+static void	check_texture_aux(t_scene *scene, char *id)
 {
 	if (ft_mtxlen(scene->mtx) != 2)
 	{
 		exit_error(get_explicit_error_message(scene,
-				(t_strs){"Invalid path to the ", ID, " texture on line ",
-				scene->line_nbr_str, NULL, NULL}), scene);
+				(t_strs){"Invalid path to the ", id, " texture on line ",
+				scene->line_nbr.str, NULL, NULL}), scene);
 	}
-	if (ft_strlen(scene->mtx[1]) < 4 || ft_strcmp(scene->mtx[1]
-			+ ft_strlen(scene->mtx[1]) - 4, ".xpm"))
+	if (ft_strlen(scene->mtx[1]) < 4 || ft_strncmp(scene->mtx[1]
+			+ ft_strlen(scene->mtx[1]) - 4, ".xpm", 4))
 	{
 		exit_error(get_explicit_error_message(scene,
-				(t_strs){"Invalid extension on the path to the ", ID,
-				" texture on line ", scene->line_nbr_str, NULL, NULL}), scene);
+				(t_strs){"Invalid extension on the path to the ", id,
+				" texture on line ", scene->line_nbr.str, NULL, NULL}), scene);
 	}
 }
 
 void	check_texture(t_scene *scene)
 {
-	char	*ID;
+	char	*id;
 
-	ID = get_element_str(*scene->line);
+	id = get_element_name(*scene->line);
 	scene->mtx = ft_split(scene->line, ' ');
-	check_texture_aux(scene, ID);
+	check_texture_aux(scene, id);
 	if (open(scene->mtx[1], O_RDONLY) < 0)
 	{
 		exit_error(get_explicit_error_message(scene,
-				(t_strs){"Invalid path to the ", ID, " texture on line ",
-				scene->line_nbr_str, ": ", strerror(errno)}), scene);
+				(t_strs){"Invalid path to the ", id, " texture on line ",
+				scene->line_nbr.str, ": ", strerror(errno)}), scene);
 	}
-	fill_texture(scene, *ID, scene->mtx[1]);
-	ft_strfree(&scene->line_nbr_str);
+	fill_texture(scene, *id, scene->mtx[1]);
+	ft_strfree(&scene->line_nbr.str);
+	ft_strfree(&scene->line_cpy);
 	ft_strfree(&scene->line);
 	ft_mtxfree(&scene->mtx);
 }
--- bonus/src/utils/error_bonus.c
+++ bonus/src/utils/error_bonus.c
@@ -6,30 +6,32 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 16:22:09 by efinda            #+#    #+#             */
-/*   Updated: 2025/03/31 17:04:14 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/24 14:58:14 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D_bonus.h"
 
-void    exit_error(char *message, t_scene *scene)
+void	exit_error(char *message, t_scene *scene)
 {
-    int i;
+	int	i;
 
-    i = write(2, "Error\n", 6);
-    i = write(2, message, ft_strlen(message));
-    i = write(2, "\n", 1);
-    ft_mtxfree(&scene->mtx);
-    ft_strfree(&scene->tmp);
-    ft_strfree(&scene->line);
-    free_rows(&scene->map.head);
-    ft_strfree(&scene->elements);
-    ft_mtxfree(&scene->map.content);
-    ft_strfree(&scene->line_nbr_str);
-    i = -1;
-    while (++i < 4)
-        ft_strfree(&scene->textures[i].path);
-    exit(1);
+	i = write(2, "Error\n", 6);
+	i = write(2, message, ft_strlen(message));
+	i = write(2, "\n", 1);
+	ft_mtxfree(&scene->mtx);
+	ft_strfree(&scene->tmp);
+	ft_strfree(&scene->line);
+	ft_strfree(&scene->line_cpy);
+	free_rows(&scene->map.head);
+	free_map_crds(&scene->map.crds);
+	ft_strfree(&scene->elements);
+	ft_mtxfree(&scene->map.content);
+	ft_strfree(&scene->line_nbr.str);
+	i = -1;
+	while (++i < 4)
+		ft_strfree(&scene->textures[i].path);
+	exit(1);
 }
 
 void	my_mlx_error_free(t_cub *cub, char *message)
