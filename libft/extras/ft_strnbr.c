698712d700a42393a773cd8a570a022bda34f168
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-04-25 10:30:02 +0100

    updated

--- libft/extras/ft_strnbr.c
+++ libft/extras/ft_strnbr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdigit.c                                      :+:      :+:    :+:   */
+/*   ft_strnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 07:21:59 by efinda            #+#    #+#             */
-/*   Updated: 2024/11/13 07:26:21 by efinda           ###   ########.fr       */
+/*   Updated: 2025/04/24 20:14:04 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ int	ft_strnbr(char *str)
 		return (0);
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
+	if (!str[i])
+		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return (0);
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (ft_isdigit(str[i]))
 		i++;
-		if (str[i] == '\0')
-			return (1);
-	}
-	return (0);
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] != '\0')
+		return (0);
+	return (1);
 }
