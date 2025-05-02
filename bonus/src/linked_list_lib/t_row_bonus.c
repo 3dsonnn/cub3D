79925f2aaef3d4d429af496cbc4fcdc31b00340f
Edson Finda <efinda@c1r4s6.42luanda.com>
2025-05-02 04:33:25 +0100

    updated the row related functions: the single node free function is now receiving only one arg and doesnt need to make all those checks which makes more sense; the vertical trim was leading me to segfault because of a silly mistake I was making on the second part of the function where I needed to start trimming from the tail of the list; and there's also the horizontal trim where I basically just added more checks and stuff

--- bonus/src/linked_list_lib/t_row_bonus.c
+++ bonus/src/linked_list_lib/t_row_bonus.c
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 16:18:27 by efinda            #+#    #+#             */
-/*   Updated: 2025/04/26 18:14:06 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/01 14:24:47 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,32 +55,15 @@ void	add_row_front(t_row **head, t_row *new)
 	}
 }
 
-void	free_row(t_row **head, t_row *ref)
+void	free_row(t_row **row)
 {
-	t_row	*cur;
-
-	if (!head || !*head || !ref)
-		return ;
-	cur = *head;
-	while (cur && cur != ref)
-		cur = cur->next;
-	if (!cur)
-		return ;
-	if (cur == *head)
+	if (row && *row)
 	{
-		*head = cur->next;
-		if (*head)
-			(*head)->prev = NULL;
+		ft_strfree(&(*row)->str);
+		ft_strfree(&(*row)->line_nbr);
+		free(*row);
+		*row = NULL;
 	}
-	else
-	{
-		cur->prev->next = cur->next;
-		if (cur->next)
-			cur->next->prev = cur->prev;
-	}
-	ft_strfree(&ref->str);
-	ft_strfree(&ref->line_nbr);
-	free(ref);
 }
 
 void	free_rows(t_row **head)
@@ -93,9 +76,7 @@ void	free_rows(t_row **head)
 	{
 		tmp = *head;
 		*head = (*head)->next;
-		ft_strfree(&tmp->str);
-		ft_strfree(&tmp->line_nbr);
-		free(tmp);
+		free_row(&tmp);
 	}
 	*head = NULL;
 }
