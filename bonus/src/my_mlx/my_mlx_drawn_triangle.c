4c5b7a9dd1709cbf18913349cb0cd0f97d6fca2b
Edson Finda <efinda@c1r4s6.42luanda.com>
2025-05-02 04:22:21 +0100

    function to draw a triangle inside an image

--- bonus/src/my_mlx/my_mlx_drawn_triangle.c
+++ bonus/src/my_mlx/my_mlx_drawn_triangle.c
@@ -1,34 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   image_manipulation_bonus.c                         :+:      :+:    :+:   */
+/*   my_mlx_drawn_triangle.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/23 14:34:02 by efinda            #+#    #+#             */
-/*   Updated: 2025/04/27 12:24:47 by efinda           ###   ########.fr       */
+/*   Created: 2025/04/29 19:54:53 by efinda            #+#    #+#             */
+/*   Updated: 2025/04/30 18:13:46 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/cub3D_bonus.h"
+#include "../../inc/my_mlx_bonus.h"
 
-void	my_mlx_draw_horizontal_lines_to_img(t_img *img, t_point crd,
-		t_point size, int color)
+void	my_mlx_drawn_triangle(t_img *img, t_point crd, int size, int color)
 {
+	int		half_base;
+	int		height;
+	int		row_width;
 	t_point	iter;
 
-	iter = (t_point){.x = -1, .y = -1};
-	if (!img || crd.x < 0 || crd.x > img->width || crd.y < 0
-		|| crd.y > img->height || size.x <= 0 || size.y <= 0)
+	if (!img || !img->addr || crd.x <= 0 || crd.y <= 0 || size <= 0)
 		return ;
-	if (crd.x + size.x > img->width)
-		size.x = img->width - crd.x;
-	if (crd.y + size.y > img->height)
-		size.y = img->height - crd.y;
-	while (++iter.y < size.y)
+	height = size;
+	half_base = size / 2;
+	iter = (t_point){-1, -1};
+	while (++iter.y < height)
 	{
-		iter.x = -1;
-		while (++iter.x < size.x)
+		row_width = (int)((double)iter.y / height * half_base);
+		iter.x = -row_width;
+		while (iter.x <= row_width)
+		{
 			my_mlx_pixel_put(img, crd.x + iter.x, crd.y + iter.y, color);
+			iter.x++;
+		}
 	}
 }
